6b607343a7e747f544429ddfbd426cfbc1256f8d
Jan Svoboda <jan@mluv.cz>
2015-10-03 01:13:58 +0200

    fix plugin reloading in development mode (route added twice)

--- init.rb
+++ init.rb
@@ -10,9 +10,10 @@ Redmine::Plugin.register :alternative_home_page do
 end
 
 ActionDispatch::Callbacks.to_prepare do
+  home_route = Rails.application.routes.named_routes.instance_variable_get('@routes')[:alternative_home_page]
   Rails.application.routes.prepend do
     root to: 'welcome#index', formats: [ :html ], as: :alternative_home_page
-  end
+  end unless home_route
 
   require_dependency 'alternative_home_page/welcome_controller_patch'
   require_dependency 'alternative_home_page/settings_controller_patch'
